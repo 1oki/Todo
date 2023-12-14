7c04a6f101a402a1c3a2b04e2468f0abd1d20c15
Sergey Filippov <41546563+1oki@users.noreply.github.com>
2023-12-15 01:35:07 +0700

    Update firebase-config.js

--- src/db/firebase-config.js
+++ src/db/firebase-config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB43NX3GLKd1YDtYcdQ_B58x81AZ-ERqeQ",
-    authDomain: "todoapp-8ae1c.firebaseapp.com",
-    databaseURL: "https://todoapp-8ae1c-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "todoapp-8ae1c",
-    storageBucket: "todoapp-8ae1c.appspot.com",
-    messagingSenderId: "405936305062",
-    appId: "1:405936305062:web:35637225a5f47872d8d567"
+
   };
   
   // Initialize Firebase
